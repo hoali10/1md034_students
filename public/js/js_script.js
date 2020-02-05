9f21605a5065a42fe1d70c499e59bdf2ec453f10
Ali <ho.ali1010@gmail.com>
2020-02-05 17:41:17 +0100

    rewrite js and vue

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,23 +1,24 @@
-function menuItem(name, caloris, lactos, gluten, img) {
+function menuItem(name, caloris, lactos, gluten, img, imgW) {
     this.name = name;
     this.caloris = caloris;
     this.lactos = lactos;
     this.gluten = gluten;
     this.img = img;
+    this.imgW = imgW;
     this.nameAndkCal = function() {
         return this.name + ' ' + this.caloris;
     };
 }
 
-let fireBurger = new menuItem('Fire Burger', '1600 kCal', 'Contains lactos', 'Contains gluten', 'https://previews.123rf.com/images/kesu87/kesu871411/kesu87141100247/33457747-hamburger-d%C3%A9licieux-avec-des-flammes-de-feu.jpg');
+let fireBurger = new menuItem('Fire Burger', '2700 kCal', 'Contains lactos', 'Contains gluten', 'https://previews.123rf.com/images/kesu87/kesu871411/kesu87141100247/33457747-hamburger-d%C3%A9licieux-avec-des-flammes-de-feu.jpg', 400);
 
-let vegan = new menuItem('Vegan', '600 kCal', 'Lactos free', 'Gluten free', 'https://joyfoodsunshine.com/wp-content/uploads/2017/04/Baked-Sweet-Potato-Black-Bean-Veggie-Burgers-gluten-free-4.jpg');
+let vegan = new menuItem('Vegan', '600 kCal', 'Lactos free', 'Gluten free', 'https://joyfoodsunshine.com/wp-content/uploads/2017/04/Baked-Sweet-Potato-Black-Bean-Veggie-Burgers-gluten-free-4.jpg', 400);
 
-let kingSize = new menuItem('King size', '3300 kCal', 'Contains lactos', 'Contains gluten', 'https://i.pinimg.com/600x315/be/2d/8c/be2d8c44cabf32bbbb90c5e541788e17.jpg');
+let kingSize = new menuItem('King size', '3300 kCal', 'Contains lactos', 'Contains gluten', 'https://i.pinimg.com/600x315/be/2d/8c/be2d8c44cabf32bbbb90c5e541788e17.jpg', 400);
 
-let daily = new menuItem('Daily', '1600 kCal', 'Lactos free', 'Contains gluten', 'https://s3.us-east-2.amazonaws.com/ordersapp/business/5660348/items/hamberger.jpg');
+let daily = new menuItem('Daily', '1600 kCal', 'Lactos free', 'Contains gluten', 'https://s3.us-east-2.amazonaws.com/ordersapp/business/5660348/items/hamberger.jpg', 400);
 
-let texa = new menuItem('Texa', '3600 kCal', 'Contains lactos', 'Contains gluten', 'https://lh3.googleusercontent.com/proxy/f9S0n_9zCVI3D6gQ4Oww8RkPg34Cwqb-pfNJZRQrN4L4oBDv_QXL7lFNvSbqKWl2rEtAeeWUxXFg3OKPk1eMzek8Bf-Gx0T2TjiNRH45BahQJDED4vHSsjyW5fIZPrLcvATxjS3BSEC3EdIwMTjIwYTgdd2_RQqFvBvBj4Fkl-u19jhT48TfgVuF-aaN7hclVmvSaCY');
+let texa = new menuItem('Texa', '3600 kCal', 'Contains lactos', 'Gluten free', 'https://dimg04.c-ctrip.com/images/100p0k000000c496wD647_C_750_350.jpg?proc=source%2Ftrip', 400);
 
 let menu = [fireBurger, vegan, kingSize, daily, texa];
 
@@ -30,25 +31,42 @@ document.getElementById('texa').innerHTML = "Texa";
 */
 /*
 let listItem = document.getElementById("wrapper1");
-
-
 for (burger of menu) {
 
     let burgerBox = document.createElement("div");
-    let allergies = '';
-
+    let burgerImg = document.createElement("img");
+    let hederItem = document.createElement("h3");
+    let listName = document.createTextNode(burger.name);
+    let burgerKcal = document.createElement("li");
+    let columnItem = document.createElement("ul");
+    let rowItem3 = document.createTextNode(burger.caloris);
+    burgerKcal.appendChild(rowItem3);
+    columnItem.appendChild(burgerKcal);
     if (burger.lactos == "Contains lactos") {
 
-        allergies += "Contains lactos";
+        let rowItem1 = document.createElement("li");
+        let rowValue1 = document.createTextNode("Contains lactos");
+        rowItem1.appendChild(rowValue1);
+        columnItem.appendChild(rowItem1);
+
 
     }
     if (burger.gluten == "Contains gluten") {
 
-        allergies += "Contains gluten";
-
+        let rowItem2 = document.createElement("li");
+        let rowValue2 = document.createTextNode("Contains gluten");
+        rowItem2.appendChild(rowValue2);
+        columnItem.appendChild(rowItem2);
     }
-    let listName = document.createTextNode(burger.name + allergies);
-    burgerBox.appendChild(listName);
+    burgerBox.appendChild(hederItem);
+    hederItem.appendChild(listName);
+    burgerImg.src = burger.img;
+    burgerImg.width = burger.imgW;
+    burgerBox.appendChild(burgerImg);
+
+
+    burgerBox.appendChild(columnItem);
     listItem.appendChild(burgerBox);
+
 }
 */
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,28 +1,3 @@
-function menuItem(name, caloris, lactos, gluten, img) {
-    this.name = name;
-    this.caloris = caloris;
-    this.lactos = lactos;
-    this.gluten = gluten;
-    this.img = img;
-    this.nameAndkCal = function() {
-        return this.name + ' ' + this.caloris;
-    };
-}
-
-
-let fireBurger = new menuItem('Fire Burger', '1600 kCal', 'Contains lactos', 'Contains gluten', 'https://previews.123rf.com/images/kesu87/kesu871411/kesu87141100247/33457747-hamburger-d%C3%A9licieux-avec-des-flammes-de-feu.jpg');
-
-let vegan = new menuItem('Vegan', '600 kCal', 'Lactos free', 'Gluten free', 'https://joyfoodsunshine.com/wp-content/uploads/2017/04/Baked-Sweet-Potato-Black-Bean-Veggie-Burgers-gluten-free-4.jpg');
-
-let kingSize = new menuItem('King size', '3300 kCal', 'Contains lactos', 'Contains gluten', 'https://i.pinimg.com/600x315/be/2d/8c/be2d8c44cabf32bbbb90c5e541788e17.jpg');
-
-let daily = new menuItem('Daily', '1600 kCal', 'Lactos free', 'Contains gluten', 'https://s3.us-east-2.amazonaws.com/ordersapp/business/5660348/items/hamberger.jpg');
-
-let texa = new menuItem('Texa', '3600 kCal', 'Contains lactos', 'Contains gluten', 'https://lh3.googleusercontent.com/proxy/f9S0n_9zCVI3D6gQ4Oww8RkPg34Cwqb-pfNJZRQrN4L4oBDv_QXL7lFNvSbqKWl2rEtAeeWUxXFg3OKPk1eMzek8Bf-Gx0T2TjiNRH45BahQJDED4vHSsjyW5fIZPrLcvATxjS3BSEC3EdIwMTjIwYTgdd2_RQqFvBvBj4Fkl-u19jhT48TfgVuF-aaN7hclVmvSaCY');
-
-let menu = [fireBurger, vegan, kingSize, daily, texa];
-
-
 const vm = new Vue({
     el: '#selection',
     data: {
