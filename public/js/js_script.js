f5ec499c9398d800db8060d4c976a91ad5b04852
Ali <ho.ali1010@gmail.com>
2020-02-05 11:55:52 +0100

    updated js and vue

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,30 +1,58 @@
-function menuItem(name, caloris, lactos, gluten) {
+function menuItem(name, caloris, lactos, gluten, img) {
     this.name = name;
     this.caloris = caloris;
     this.lactos = lactos;
     this.gluten = gluten;
+    this.img = img;
     this.nameAndkCal = function() {
         return this.name + ' ' + this.caloris;
     };
 }
 
-let fireBurgerImg = 'https://previews.123rf.com/images/kesu87/kesu871411/kesu87141100247/33457747-hamburger-d%C3%A9licieux-avec-des-flammes-de-feu.jpg" width="400" '
-let fireBurger = new menuItem('Fire Burger', '1600 kCal', 'Contains lactos', 'Contains gluten');
+let fireBurger = new menuItem('Fire Burger', '1600 kCal', 'Contains lactos', 'Contains gluten', 'https://previews.123rf.com/images/kesu87/kesu871411/kesu87141100247/33457747-hamburger-d%C3%A9licieux-avec-des-flammes-de-feu.jpg');
 
-let veganImg = 'https://joyfoodsunshine.com/wp-content/uploads/2017/04/Baked-Sweet-Potato-Black-Bean-Veggie-Burgers-gluten-free-4.jpg" width="300"'
-let vegan = new menuItem('Vegan', '600 kCal', 'Contains lactos', 'Contains gluten');
+let vegan = new menuItem('Vegan', '600 kCal', 'Lactos free', 'Gluten free', 'https://joyfoodsunshine.com/wp-content/uploads/2017/04/Baked-Sweet-Potato-Black-Bean-Veggie-Burgers-gluten-free-4.jpg');
 
-let kingSizeImg = 'https://i.pinimg.com/600x315/be/2d/8c/be2d8c44cabf32bbbb90c5e541788e17.jpg" width="300"'
-let kingSize = new menuItem('King size', '3300 kCal', 'Contains lactos', 'Contains gluten');
+let kingSize = new menuItem('King size', '3300 kCal', 'Contains lactos', 'Contains gluten', 'https://i.pinimg.com/600x315/be/2d/8c/be2d8c44cabf32bbbb90c5e541788e17.jpg');
 
-let dailyImg = 'https://s3.us-east-2.amazonaws.com/ordersapp/business/5660348/items/hamberger.jpg'
-let daily = new menuItem('Daily', '1600 kCal', 'Lactos free', 'Contains gluten');
+let daily = new menuItem('Daily', '1600 kCal', 'Lactos free', 'Contains gluten', 'https://s3.us-east-2.amazonaws.com/ordersapp/business/5660348/items/hamberger.jpg');
 
-let texaImg = 'https://lh3.googleusercontent.com/proxy/f9S0n_9zCVI3D6gQ4Oww8RkPg34Cwqb-pfNJZRQrN4L4oBDv_QXL7lFNvSbqKWl2rEtAeeWUxXFg3OKPk1eMzek8Bf-Gx0T2TjiNRH45BahQJDED4vHSsjyW5fIZPrLcvATxjS3BSEC3EdIwMTjIwYTgdd2_RQqFvBvBj4Fkl-u19jhT48TfgVuF-aaN7hclVmvSaCY'
-let texa = new menuItem('Texa', '3600 kCal', 'Contains lactos', 'Contains gluten');
+let texa = new menuItem('Texa', '3600 kCal', 'Contains lactos', 'Contains gluten', 'https://lh3.googleusercontent.com/proxy/f9S0n_9zCVI3D6gQ4Oww8RkPg34Cwqb-pfNJZRQrN4L4oBDv_QXL7lFNvSbqKWl2rEtAeeWUxXFg3OKPk1eMzek8Bf-Gx0T2TjiNRH45BahQJDED4vHSsjyW5fIZPrLcvATxjS3BSEC3EdIwMTjIwYTgdd2_RQqFvBvBj4Fkl-u19jhT48TfgVuF-aaN7hclVmvSaCY');
+
+let menu = [fireBurger, vegan, kingSize, daily, texa];
 
 /*
 document.getElementById('fireBurger').innerHTML = "Fire Burger";
 document.getElementById('vegan').innerHTML = "Vegan";
 document.getElementById('kingSize').innerHTML = "King Size";
+document.getElementById('daily').innerHTML = "Daily";
+document.getElementById('texa').innerHTML = "Texa";
+*/
+
+/*
+let listItem = document.getElementById("wrapper1");
+
+
+for (let i = 0; i < menu.length; i++) {
+
+    let burgerBox = document.createElement("div");
+    let listName = document.createTextNode(menu[i].name);
+    burgerBox.appendChild(listName);
+
+
+    if (menu[i].lactos == "Contains lactos") {
+        let listLactos = document.createTextNode(menu[i].lactos);
+        burgerBox.appendChild(listLactos);
+
+    }
+    if (menu[i].gluten == "Contains gluten") {
+
+        let listGluten = document.createTextNode(menu[i].gluten);
+        burgerBox.appendChild(listGluten);
+
+
+    }
+    listItem.appendChild(burgerBox);
+}
+
 */
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,5 +1,18 @@
+function menuItem(name, caloris, lactos, gluten, img) {
+    this.name = name;
+    this.caloris = caloris;
+    this.lactos = lactos;
+    this.gluten = gluten;
+    this.img = img;
+    this.nameAndkCal = function() {
+        return this.name + ' ' + this.caloris;
+    };
+}
+
+let menu = [fireBurger, vegan, kingSize, daily, texa];
+
 const vm = new Vue({
-    el: '#selection',
+    el: '#wrapper1',
     data: {
         burgerSelection: 'Välj en burgare' + ' ' + new Date(),
         fireBurger: fireBurger,
@@ -7,6 +20,7 @@ const vm = new Vue({
         kingSize: kingSize,
         daily: daily,
         texa: texa,
-
+        booleanExpression: true,
+        burgers: menu,
     }
 })
